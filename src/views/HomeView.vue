0a56331300f3af590f90a367ce5ebe83da2fb9c4
Emir Pozzi <emir.pozzi@valtech.com>
2022-06-13 14:35:54 +0200

    Do first implementation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <label for="input">Write a name: </label>
+  <input v-model="input" type="text" />
+  <button @click="getStatistics(input)">Get Stats</button>
+  <p>Name is: {{ this.name }}</p>
+  <p>Age is: {{ this.age }}</p>
+  <p>Gender is: {{ this.gender }}</p>
+  <ol>
+    <li v-for="country in countryList" :key="country.country_id">
+      {{ country.country_id }} - {{ country.probability }}
+    </li>
+  </ol>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      input: "",
+      age: 0,
+      name: "",
+      gender: "",
+      countryList: [],
+    };
+  },
+  methods: {
+    async getAge(name: string) {
+      const result = await fetch(`https://api.agify.io/?name=${name}`);
+      const data = await result.json();
+      this.age = data.age;
+    },
+    async getGender(name: string) {
+      const result = await fetch(`https://api.genderize.io/?name=${name}`);
+      const data = await result.json();
+      this.gender = data.gender;
+    },
+    async getNationality(name: string) {
+      const result = await fetch(`https://api.nationalize.io/?name=${name}`);
+      const data = await result.json();
+      this.countryList = data.country;
+      console.log(data);
+    },
+    async getStatistics(name: string) {
+      this.name = name;
+      this.getAge(name);
+      this.getGender(name);
+      this.getNationality(name);
+    },
   },
 });
 </script>
